3c43a75e1bf44bd1d8becf049f4e533c93a887d7
MathieuLeroy2 <mathieu.leroy@vives.be>
2026-03-31 19:00:12 +0200

    added order page and logic

--- orders/urls.py
+++ orders/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import dashboard
+from .views import dashboard, order_list
 
 app_name = "orders"
 
 urlpatterns = [
     path("", dashboard, name="dashboard"),
+    path("orders/", order_list, name="order_list"),
 ]
--- orders/views.py
+++ orders/views.py
@@ -1,8 +1,16 @@
-from django.shortcuts import render
-
-# Create your views here.
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from .models import Order
+
+
+def get_visible_orders_for_user(user):
+    if user.role == "admin":
+        return Order.objects.select_related("user", "decided_by").all()
+
+    if user.role == "teacher":
+        return Order.objects.select_related("user", "decided_by").filter(user=user)
+
+    return Order.objects.select_related("user", "decided_by").filter(user=user)
 
 
 @login_required
@@ -13,3 +21,13 @@ def dashboard(request):
         "user": user,
     }
     return render(request, "orders/dashboard.html", context)
+
+
+@login_required
+def order_list(request):
+    orders = get_visible_orders_for_user(request.user)
+
+    context = {
+        "orders": orders,
+    }
+    return render(request, "orders/order_list.html", context)
